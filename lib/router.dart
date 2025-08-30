11952901b9a79261eb56fdc54dda25e654941415
eliassilveirajr <eliassilveirajr@gmail.com>
2025-08-30 21:34:18 +0000

    feat: Adicionando tela de perfil, telas básicas, alterando nome do app e ícone para geração do apk

--- lib/router.dart
+++ lib/router.dart
@@ -3,11 +3,12 @@ import 'package:go_router/go_router.dart';
 import 'package:myapp/cadastro_screen.dart';
 import 'package:myapp/home_screen.dart';
 import 'package:myapp/login_screen.dart';
+import 'package:myapp/profile_screen.dart'; // Corrigido: importando o arquivo correto
 import 'package:myapp/splash_screen.dart';
 
 // Configuração do GoRouter
 final GoRouter router = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/', // Começa na SplashScreen
   routes: <RouteBase>[
     GoRoute(
       path: '/',
@@ -27,11 +28,22 @@ final GoRouter router = GoRouter(
         return const CadastroScreen();
       },
     ),
+    // A HomeScreen é a rota principal após o login
     GoRoute(
       path: '/home',
       builder: (BuildContext context, GoRouterState state) {
         return const HomeScreen();
       },
+      // A tela de perfil é uma sub-rota da home
+      routes: [
+        GoRoute(
+          path: 'perfil', // O path é relativo: /home/perfil
+          builder: (BuildContext context, GoRouterState state) {
+            // Corrigido: usando a tela correta
+            return const ProfileScreen();
+          },
+        ),
+      ]
     ),
   ],
 );
