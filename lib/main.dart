a16e8578d18c1b063b749da7a5cf26db3c7f0fe2
eliassilveirajr <eliassilveirajr@gmail.com>
2025-08-30 23:40:42 +0000

    feat: criando telas de andamentos e relacionados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/router.dart'; // Importe o router
 import 'package:provider/provider.dart';
@@ -97,6 +98,17 @@ class MyApp extends StatelessWidget {
           darkTheme: darkTheme,
           themeMode: themeProvider.themeMode,
           debugShowCheckedModeBanner: false,
+          // Configuração de localização para Português do Brasil
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('pt', 'BR'),
+            Locale('en', 'US'), // Adicionado para referência
+          ],
+          locale: const Locale('pt', 'BR'), // Define o locale padrão
         );
       },
     );
